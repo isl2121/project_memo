050006a792514118fb0d17322e6195b2f2d6d812
isl2121 <isl2121@naver.com>
2018-11-30 18:32:31 +0900

    allowd_host change

---
+++ memo/foms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Memo
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+class MemoForm(forms.ModelForm):
+    class Meta:
+        model = Memo
+        fields = ['title','content']
